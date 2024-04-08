4b08b5d1fa2eadcf5361c8337f67ec8e1fb527fa
Quackity1 <83092295+Quackity1@users.noreply.github.com>
2024-04-08 19:51:08 -0400

    Update AdditionalPersonDetails.kt

--- app/android/android-passport-reader/app/src/main/java/example/jllarraz/com/passportreader/data/AdditionalPersonDetails.kt
+++ app/android/android-passport-reader/app/src/main/java/example/jllarraz/com/passportreader/data/AdditionalPersonDetails.kt
@@ -3,9 +3,6 @@ package example.jllarraz.com.passportreader.data
 import android.os.Parcel
 import android.os.Parcelable
 
-import java.util.ArrayList
-import java.util.Date
-
 class AdditionalPersonDetails : Parcelable {
 
     var custodyInformation: String? = null
@@ -33,44 +30,27 @@ class AdditionalPersonDetails : Parcelable {
     }
 
     constructor(`in`: Parcel) {
-
         otherNames = ArrayList()
         otherValidTDNumbers = ArrayList()
         permanentAddress = ArrayList()
         placeOfBirth = ArrayList()
         tagPresenceList = ArrayList()
 
-        this.custodyInformation = if (`in`.readInt() == 1) `in`.readString() else null
-        this.fullDateOfBirth = if (`in`.readInt() == 1) `in`.readString() else null
-        this.nameOfHolder = if (`in`.readInt() == 1) `in`.readString() else null
-        if (`in`.readInt() == 1) {
-            `in`.readList(otherNames!!, String::class.java.classLoader)
-        }
-        if (`in`.readInt() == 1) {
-            `in`.readList(otherValidTDNumbers!!, String::class.java.classLoader)
-        }
-        if (`in`.readInt() == 1) {
-            `in`.readList(permanentAddress!!, String::class.java.classLoader)
-        }
-        this.personalNumber = if (`in`.readInt() == 1) `in`.readString() else null
-        this.personalSummary = if (`in`.readInt() == 1) `in`.readString() else null
-        if (`in`.readInt() == 1) {
-            `in`.readList(placeOfBirth!!, String::class.java.classLoader)
-        }
-        this.profession = if (`in`.readInt() == 1) `in`.readString() else null
-        if (`in`.readInt() == 1) {
-            this.proofOfCitizenship = ByteArray(`in`.readInt())
-            `in`.readByteArray(this.proofOfCitizenship!!)
-        }
-        tag = `in`.readInt()
-        if (`in`.readInt() == 1) {
-            `in`.readList(tagPresenceList!!, Int::class.java.classLoader)
-        }
-
-        this.telephone = if (`in`.readInt() == 1) `in`.readString() else null
-        this.title = if (`in`.readInt() == 1) `in`.readString() else null
-
-
+        this.custodyInformation = `in`.readNullableString()
+        this.fullDateOfBirth = `in`.readNullableString()
+        this.nameOfHolder = `in`.readNullableString()
+        this.otherNames = `in`.readNullableList()
+        this.otherValidTDNumbers = `in`.readNullableList()
+        this.permanentAddress = `in`.readNullableList()
+        this.personalNumber = `in`.readNullableString()
+        this.personalSummary = `in`.readNullableString()
+        this.placeOfBirth = `in`.readNullableList()
+        this.profession = `in`.readNullableString()
+        this.proofOfCitizenship = `in`.readNullableByteArray()
+        this.tag = `in`.readInt()
+        this.tagPresenceList = `in`.readNullableList()
+        this.telephone = `in`.readNullableString()
+        this.title = `in`.readNullableString()
     }
 
     override fun describeContents(): Int {
@@ -78,83 +58,48 @@ class AdditionalPersonDetails : Parcelable {
     }
 
     override fun writeToParcel(dest: Parcel, flags: Int) {
-        dest.writeInt(if (custodyInformation != null) 1 else 0)
-        if (custodyInformation != null) {
-            dest.writeString(custodyInformation)
-        }
-
-        dest.writeInt(if (fullDateOfBirth != null) 1 else 0)
-        if (fullDateOfBirth != null) {
-            dest.writeString(fullDateOfBirth)
-        }
-
-
-        dest.writeInt(if (nameOfHolder != null) 1 else 0)
-        if (nameOfHolder != null) {
-            dest.writeString(nameOfHolder)
-        }
-        dest.writeInt(if (otherNames != null) 1 else 0)
-        if (otherNames != null) {
-            dest.writeList(otherNames)
-        }
-
-        dest.writeInt(if (otherValidTDNumbers != null) 1 else 0)
-        if (otherValidTDNumbers != null) {
-            dest.writeList(otherValidTDNumbers)
-        }
-
-        dest.writeInt(if (permanentAddress != null) 1 else 0)
-        if (permanentAddress != null) {
-            dest.writeList(permanentAddress)
-        }
-
-        dest.writeInt(if (personalNumber != null) 1 else 0)
-        if (personalNumber != null) {
-            dest.writeString(personalNumber)
-        }
-
-        dest.writeInt(if (personalSummary != null) 1 else 0)
-        if (personalSummary != null) {
-            dest.writeString(personalSummary)
-        }
-
-        dest.writeInt(if (placeOfBirth != null) 1 else 0)
-        if (placeOfBirth != null) {
-            dest.writeList(placeOfBirth)
-        }
-
-        dest.writeInt(if (profession != null) 1 else 0)
-        if (profession != null) {
-            dest.writeString(profession)
-        }
-
-        dest.writeInt(if (proofOfCitizenship != null) 1 else 0)
-        if (proofOfCitizenship != null) {
-            dest.writeInt(proofOfCitizenship!!.size)
-            dest.writeByteArray(proofOfCitizenship)
-        }
-
+        writeNullableString(dest, custodyInformation)
+        writeNullableString(dest, fullDateOfBirth)
+        writeNullableString(dest, nameOfHolder)
+        writeNullableList(dest, otherNames)
+        writeNullableList(dest, otherValidTDNumbers)
+        writeNullableList(dest, permanentAddress)
+        writeNullableString(dest, personalNumber)
+        writeNullableString(dest, personalSummary)
+        writeNullableList(dest, placeOfBirth)
+        writeNullableString(dest, profession)
+        writeNullableByteArray(dest, proofOfCitizenship)
         dest.writeInt(tag)
-        dest.writeInt(if (tagPresenceList != null) 1 else 0)
-        if (tagPresenceList != null) {
-            dest.writeList(tagPresenceList)
-        }
+        writeNullableList(dest, tagPresenceList)
+        writeNullableString(dest, telephone)
+        writeNullableString(dest, title)
+    }
 
-        dest.writeInt(if (telephone != null) 1 else 0)
-        if (telephone != null) {
-            dest.writeString(telephone)
+    private fun writeNullableString(dest: Parcel, value: String?) {
+        dest.writeInt(if (value != null) 1 else 0)
+        if (value != null) {
+            dest.writeString(value)
         }
+    }
 
-        dest.writeInt(if (title != null) 1 else 0)
-        if (title != null) {
-            dest.writeString(title)
+    private fun writeNullableList(dest: Parcel, list: List<String>?) {
+        dest.writeInt(if (list != null) 1 else 0)
+        if (list != null) {
+            dest.writeList(list)
         }
+    }
 
+    private fun writeNullableByteArray(dest: Parcel, byteArray: ByteArray?) {
+        dest.writeInt(if (byteArray != null) 1 else 0)
+        if (byteArray != null) {
+            dest.writeInt(byteArray.size)
+            dest.writeByteArray(byteArray)
+        }
     }
 
     companion object {
         @JvmField
-        val CREATOR: Parcelable.Creator<*> = object : Parcelable.Creator<AdditionalPersonDetails> {
+        val CREATOR: Parcelable.Creator<AdditionalPersonDetails> = object : Parcelable.Creator<AdditionalPersonDetails> {
             override fun createFromParcel(pc: Parcel): AdditionalPersonDetails {
                 return AdditionalPersonDetails(pc)
             }
