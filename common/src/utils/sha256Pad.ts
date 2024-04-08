ea7ad96da806fcb8b47c3a1e5f94a6da0c841338
Quackity1 <83092295+Quackity1@users.noreply.github.com>
2024-04-08 19:56:42 -0400

    Update sha256Pad.ts

--- common/src/utils/sha256Pad.ts
+++ common/src/utils/sha256Pad.ts
@@ -32,7 +32,7 @@ export function int64toBytes(num: number): Uint8Array {
 
 export function mergeUInt8Arrays(a1: Uint8Array, a2: Uint8Array): Uint8Array {
   // sum of individual array lengths
-  var mergedArray = new Uint8Array(a1.length + a2.length);
+ const mergedArray = new Uint8Array(a1.length + a2.length);
   mergedArray.set(a1);
   mergedArray.set(a2, a1.length);
   return mergedArray;
